8f7c1fcd49418fe60779ba74e77ccd57c874cbc2
Your Name <you@example.com>
2022-03-13 14:32:04 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -12,7 +12,7 @@ y en c almacena el valor de la suma de a más b.
 */
 
 void suma(int a, int b, int * c) {
-    
+    *c = a+b;
 }
 
 /*
@@ -37,7 +37,11 @@ Utilice la función sumaN.
 */
 
 void sumaNultimos(int a[], int n, int m, int * suma) {
-
+   int primeraPosicion = 0;
+   primeraPosicion = n - m;
+   for(int i = primeraPosicion ; i < n ; i++){
+      *suma += a[i];
+   }
 }
 
 /*
